6e8e1da26676a6139d3d269f864db6885f1df3ab
Alexander Pletnev <pletnev.a.a@yandex.ru>
2021-04-27 20:47:54 +0300

    Накидал пару блоков. Подключил всё что нужно для работы

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	const catalogSlider = document.querySelector('.catalog-slider');
+	if (catalogSlider) {
+		let catalogSwiper = new Swiper(catalogSlider, {
+			// Optional parameters
+			slidesPerView: 3,
+			spaceBetween: 80,
+			loop: true,
+		
+			// Navigation arrows
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+
+		})
+	}
 
 })
